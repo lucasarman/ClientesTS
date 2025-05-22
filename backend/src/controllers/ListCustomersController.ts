352b35b2f5b93ffaf162a4ece60123619fcd80a5
Arman <87951618+lucasarman@users.noreply.github.com>
2025-05-21 23:17:51 -0300

    trainingts

---
+++ backend/src/controllers/ListCustomersController.ts
@@ -0,0 +1,14 @@
+import { FastifyRequest, FastifyReply } from "fastify";
+import { ListCustomersServices } from "../services/ListCustomersServices";
+
+class ListCustomersController {
+  async handle(request: FastifyRequest, reply: FastifyReply) {
+    const listCustomerService = new ListCustomersServices();
+
+    const customers = await listCustomerService.execute();
+
+    reply.send(customers);
+  }
+}
+
+export { ListCustomersController };
